65e55f6b40b8952a77bde20b139eb69e0d5d1ef6
Deniz Alatas <wtx684@haw-hamburg.de>
2023-12-22 13:04:03 +0100

    auswertung der leitungsmess csv hinzugefügt

--- ausw_mess_leitung.py
+++ ausw_mess_leitung.py
@@ -1,21 +1,17 @@
-#tests zur vorbereitung der abspeicherung der daten in csv dateien
-
-#import csv
-#from datetime import date
 import time
-import numpy as np
+#import numpy as np
 import pandas as pd
-import matplotlib as plt
+#import matplotlib as plt
 
 path_to_csv_file="C:/Users/deniz/Masterthesis_git/python_skripte/python_skripte/csv"
 file_name="/datei"
 file_end=".csv"
 hallo="hallo"
 saved_data="/Dataset_test.csv"
-#time_now=date.ctime()
+
 
 gesammtname=path_to_csv_file+file_name+time.strftime("%d.%m.%Y_%X")+file_end
-#print(gesammtname)
+
 
 data_csv=pd.read_csv(path_to_csv_file+saved_data,header=0,skiprows=43,usecols=[0,1],sep=';',decimal=',')
 
