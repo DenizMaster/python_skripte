20c330f4ded5a7e74e854e529996a2c4a5eafc1e
Deniz Alatas <deniz-suekrue.alatas@haw-hamburg.de>
2024-01-05 17:15:33 +0100

    anpassung in verbundung - fertig zu testen/namensaenderung bei "lora empfanger"/zipdatei ist zu ignorieren

--- lora empfanger.py
+++ lora empfanger.py
@@ -18,14 +18,18 @@ import serial
 import time
 import datetime as dt
 import csv
+import os
 
 ser = serial.Serial('/dev/ttyACM0', baudrate=115200)
 
 
 time_now_dateiname_str=time.strftime("%d-%m-%Y_%X")
 counter=0
-kopf_der_spalten="kopf der spalten"#TODO muss noch angepasst werden
-dateiname = '~/Dokumente/CSV_datei/messwerte '+time_now_dateiname_str+'.csv'
+kopf_der_spalten=["Index","RSSI","SNR"]
+#dateiname = '~/Dokumente/CSV_datei/testdaten '+time_now_dateiname_str+'.csv'
+dateiname = '~/testdaten '+time_now_dateiname_str+'.csv'
+
+os.system("> "+dateiname)
 datenbank = open(dateiname,mode='w',newline='\n')
 csv.writer(datenbank).writerow(kopf_der_spalten)
 
@@ -34,7 +38,6 @@ input_1=ser.write(b'reboot\n')
 print("sleep start")
 time.sleep(1)
 print("sleep end")
-#ser.write(b'sx1280 rx start')
 time.sleep(0.5)
 ser.write(b'sx1280 rx start\n')
 time.sleep(0.5)
--- ssh verbundung.py
+++ ssh verbundung.py
@@ -6,18 +6,39 @@ Created on Tue Nov 28 12:59:14 2023
 @author: lora
 """
 
-from fabric import Connection
-#import serial
+import fabric 
+import serial
+
 import os
-HOSTS=['10.42.0.1','10.42.0.2']
+HOSTS=['10.42.0.1','10.42.0.78']
 
-test_cmd = "ping " + HOSTS[0]
+test_cmd = "ping " + HOSTS[1]
 
 
-c1=Connection(HOSTS[0])
-c1.run("cd ~/Documents/")
-c1.run("~/Documents/ > testdatei_123.csv")
-os.system("scp lora@10.42.0.1:~/Documents/testdatei_123.csv ~/Dokumente")
+c1=fabric.Connection(HOSTS[0])
+#for i in range(100):
+#    recl=c1.run(test_cmd)
+#    print(recl)
+#    print(i)
+
+#recl=c1.run("pwd")
+#print(recl)
+#recl=c1.run("> testdatei_12345.csv")
+recl=c1.run("ls")
+#print(recl)
+daten=str(recl).split("\n")
+print(daten)
+messdaten_liste=[]
+for i in daten:
+    if i.startswith("testdatei"):
+        messdaten_liste.append(i)
+
+print(messdaten_liste)
+#recl=c1.run("pwd")
+#print(recl)
+#c1.run("~/Documents > testdatei_123.csv")
+for i in messdaten_liste:
+    os.system("scp lora@10.42.0.1:~/"+i+" ~/Dokumente")
 #c1.run("python3 'lora\ empfänger.py'")
 #output=c1.run("ip addr\n")
 #print(output)
