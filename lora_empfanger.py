fdfdd4715e0a76ed11324e08aa18eb62ee5f9c97
Deniz Alatas <deniz-suekrue.alatas@haw-hamburg.de>
2024-01-05 18:13:18 +0100

    fehler mit datentypen getestet

--- lora_empfanger.py
+++ lora_empfanger.py
@@ -46,12 +46,13 @@ while counter<10:
     #output= ser.read(size=10)
     #output= ser.read_until(expected=b']',size=18)
     time_now_timestamp=time.strftime("%X_%x")#dt.datetime.now()
-    output= ser.readline()
-    #output=output.split("X")
-    #output_list=output.append(time_now_timestamp)
+    output= str(ser.readline())
+    output=output.split("X")
+    output_list=output.append(time_now_timestamp)
     #csv.writer(datenbank).writerow(output_list)
     print(output)
     print(type(output))
+    print(output_list)
     if counter==10:
         print("if in while")
         counter=0
