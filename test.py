099dd4515bdcda2a3e8eed5586483891483ac148
Deniz Alatas <deniz-suekrue.alatas@haw-hamburg.de>
2024-01-09 09:27:48 +0100

    testdateien zur testung

--- test.py
+++ test.py
@@ -1,43 +1,20 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov  9 15:01:04 2023
 
-@author: lora
-"""
-
-#import os
+import fabric
 import serial
 import time
-#import threading
-def wait_2_send():
-    trigger=b'<'
-    output=1
-    while output!=trigger:
-        output= ser.read(size=1)
-        print("wait_2 send - while")
-        
+import os
+from threading import Thread
 
-ser = serial.Serial('/dev/ttyACM0', baudrate=115200)
 
-#stream = os.popen('BOARD=nucleo-l073rz make -C ~/RIOT/tests/driver_sx1280 term')
-#output= stream.read()
-#print(output)
 
-input_1=ser.write(b'reboot')
-#wait_2_send()
-print("sleep start")
+HOSTS=['10.42.0.1','10.42.0.78']
+c1=fabric.Connection(HOSTS[0])
 
-time.sleep(1)
-print("sleep end")
-ser.write(b'sx1280 rx start')
-time.sleep(0.5)
-while True:
-    #print("while oben")
-    #output= ser.read(size=1)
-    #output= ser.read_until(expected=b']',size=18)
-    output= ser.readlines()
-    print(output)
-    print("lauft")
 
-ser.close()
+#def ssh_verbindung():
+      
+c1.run("python3 ~/pythonscript/python_skripte/test2.py")
+    #messdaten_liste=[]
+    #while True:
+     #   recl=c1.run("ls ~/pythonscript/python_skripte")
+      #  daten=str(recl).split("\n")
--- test2.py
+++ test2.py
@@ -1,22 +1,24 @@
-import csv
-
+#import csv
+import os
 # Daten zum Schreiben in die CSV-Datei
-data = [
-    ['Name', 'Alter', 'Stadt'],
-    ['Max Mustermann', 25, 'Berlin'],
-    ['Anna Schmidt', 30, 'Hamburg'],
-    ['Paul Müller', 28, 'München']
-]
+#data = [
+#    ['Name', 'Alter', 'Stadt'],
+#    ['Max Mustermann', 25, 'Berlin'],
+#    ['Anna Schmidt', 30, 'Hamburg'],
+#    ['Paul Müller', 28, 'München']
+#]
 
 # Dateipfad für die CSV-Datei
-csv_file_path = '/beispiel.csv'
-path_to_csv_file="C:/Users/deniz/Masterthesis_git/python_skripte/python_skripte/csv"
+##csv_file_path = '/beispiel.csv'
+#path_to_csv_file="C:/Users/deniz/Masterthesis_git/python_skripte/python_skripte/csv"
 # CSV-Datei schreiben
-with open(path_to_csv_file+csv_file_path, mode='w', newline='') as file:
-    writer = csv.writer(file)
+#with open(path_to_csv_file+csv_file_path, mode='w', newline='') as file:
+#    writer = csv.writer(file)
     
     # Schreibe die Daten in die CSV-Datei
-    for row in data:
-        writer.writerow(row)
+#    for row in data:
+#        writer.writerow(row)
 
-print(f'Die CSV-Datei wurde erfolgreich erstellt: {csv_file_path}')
+#print(f'Die CSV-Datei wurde erfolgreich erstellt: {csv_file_path}')
+
+os.system("> ~/testdatei_ausfuehrungpythonfileueberssh.txt")
